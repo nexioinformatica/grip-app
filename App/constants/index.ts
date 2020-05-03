f6d9e6838ecc2b2dcde16b484cf7c9454af2b805
lparolari <luca.parolari23@gmail.com>
2020-05-03 14:38:11 +0200

    Fix self signed certificate error

--- App/constants/index.ts
+++ App/constants/index.ts
@@ -1,4 +1,4 @@
 export const API_KEY = "cc96d24a-c4af-4774-a7a6-4136fb07c718";
 
-// export const BASE_URL = "https://api.agilecom.it:8443";
-export const BASE_URL = "http://localhost:3000";
+export const BASE_URL = "https://api.agilecom.it:8443";
+// export const BASE_URL = "http://localhost:3000";
