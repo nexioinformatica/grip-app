909356d59f66ac0624c21703a93e61587605ae51
lparolari <luca.parolari23@gmail.com>
2020-06-15 23:00:01 +0200

    Add persistent login behaviour

--- App/stores/auth.tsx
+++ App/stores/auth.tsx
@@ -1,15 +1,22 @@
-import React, { createContext, useState, useContext } from "react";
-
+import React, { createContext, useState, useContext, useEffect } from "react";
+import { AsyncStorage } from "react-native";
 import moment from "moment";
-
 import { pipe } from "fp-ts/lib/pipeable";
 import * as E from "fp-ts/lib/Either";
-
 import { noop } from "../util/noop";
-import { ErrorContext } from "./error";
 import { Login, Token, User } from "../types";
 import { token as tokenApi } from "../data/auth";
+import { ErrorContext } from "./error";
 
+const USER_STORAGE_KEY = "user";
+
+/** Json reviver for user object. */
+const userReviver = (k: string, v: any) => {
+  if (k === "timestamp") return moment(v);
+  return v;
+};
+
+/** The Auth Context type */
 interface Context {
   user?: User;
   logout: () => void;
@@ -17,6 +24,7 @@ interface Context {
   // auth: () => void;
 }
 
+/** @returns A user with given username, a timestamp and a token. */
 const buildUser = (username: string) => (token: Token): User => {
   return {
     username: username,
@@ -29,7 +37,6 @@ export const AuthContext = createContext<Context>({
   user: undefined,
   logout: noop,
   login: noop,
-  // auth: noop,
 });
 
 export function AuthContextProvider({
@@ -37,19 +44,49 @@ export function AuthContextProvider({
 }: {
   children: JSX.Element;
 }): React.ReactElement {
+  const { setError } = useContext(ErrorContext);
   const [user, setUser] = useState<undefined | User>(undefined);
 
-  const { setError } = useContext(ErrorContext);
+  useEffect(() => {
+    // Try to retrieve and decode the user saved in the async storage, if any.
+    (async () => {
+      try {
+        const s = await AsyncStorage.getItem(USER_STORAGE_KEY);
+        const u = JSON.parse(s || "", userReviver);
 
+        if (!u) setUser(undefined);
+        else setUser(u);
+      } catch (err) {
+        setUser(undefined);
+      }
+    })();
+  }, []);
+
+  useEffect(() => {
+    // Update the async storage according the modifications done to the user object in te state,
+    // i.e., update the user with it's json string or delete the key if user is undefined.
+    (async () => {
+      try {
+        if (!user) AsyncStorage.removeItem(USER_STORAGE_KEY);
+        else await AsyncStorage.setItem(USER_STORAGE_KEY, JSON.stringify(user));
+      } catch (err) {
+        setError(err);
+      }
+    })();
+  }, [user]);
+
+  /** Login the user either with login or token data. */
   const login = (data: E.Either<Login, Token>) =>
     pipe(
       data,
       E.fold(
-        (data) =>
-          pipe(tokenApi(E.left(data)), buildUser(data.username), setUser),
-        (data) => 1
+        (login) =>
+          pipe(tokenApi(E.left(login)), buildUser(login.username), setUser),
+        (token) => 1 // TODO: is this useful? Note that we save in the async storage the whole user object not only the token.
       )
     );
+
+  /** Logout the user. */
   const logout = () => setUser(undefined);
 
   return (
