0aeac386b1a2f670bc426203663af949d7b522f4
lparolari <luca.parolari23@gmail.com>
2020-06-17 22:27:26 +0200

    Update app secrets

--- App/constants/index.ts
+++ App/constants/index.ts
@@ -1,3 +1,5 @@
+import Constants from "expo-constants";
+
 export const API_KEY = "cc96d24a-c4af-4774-a7a6-4136fb07c718";
 
 export const BASE_URL = "https://api.agilecom.it:8443";
@@ -5,3 +7,28 @@ export const BASE_URL = "https://api.agilecom.it:8443";
 
 export const SECOND_MS = 1000;
 export const API_CLIENT_TIMEOUT = 10 * SECOND_MS;
+
+/**
+ * The current release channel. Can be one of `development`, `default` or
+ * `production-v{version}`.
+ */
+export const RELEASE_CHANNEL: string =
+  Constants.manifest.releaseChannel || "development";
+
+/**
+ * Whether we're running a staging version of the app
+ */
+export const IS_STAGING = RELEASE_CHANNEL === "default";
+
+/**
+ * Whether we're running a production version of the app
+ */
+export const IS_PROD = RELEASE_CHANNEL.startsWith("production-v");
+
+/**
+ * Whether or not we should set up Sentry bug tracking
+ */
+export const IS_SENTRY_SET_UP =
+  // We also added sentry on staging btw
+  (IS_PROD || IS_STAGING) &&
+  typeof Constants.manifest.extra.sentryPublicDsn === "string";
---
+++ scripts/generateAppJson.js
@@ -0,0 +1,39 @@
+#!/usr/bin/env node
+
+/**
+ * This script will take the `app.example.json` file from the root folder, and
+ * override the default values with ones that are in the environment variables.
+ * The output will be `app.json` in the root folder.
+ */
+
+/* eslint-disable @typescript-eslint/no-var-requires */
+
+require("dotenv").config();
+const jsonfile = require("jsonfile");
+const merge = require("lodash/merge");
+
+const defaultAppJson = require("../app.example.json");
+const pkgJson = require("../package.json");
+
+const overrides = {
+  expo: {
+    extra: {
+      sentryPublicDsn: process.env.SIS_SENTRY_PUBLIC_DNS || null,
+    },
+    // hooks: {
+    //   postPublish: [
+    //     {
+    //       file: 'sentry-expo/upload-sourcemaps',
+    //       config: {
+    //         organization: process.env.SIS_SENTRY_ORG || null,
+    //         project: process.env.SIS_SENTRY_PROJECT || null,
+    //         authToken: process.env.SIS_SENTRY_AUTH_TOKEN || null
+    //       }
+    //     }
+    //   ]
+    // },
+    version: pkgJson.version,
+  },
+};
+
+jsonfile.writeFileSync("app.json", merge(defaultAppJson, overrides));
